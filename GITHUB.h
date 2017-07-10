0511eab89e4ed88be3f83ab1e76426c26e489905
MAK_midas <muzafar.alikhan15@gmail.com>
2017-07-10 19:22:03 +0530

    updated

--- GITHUB.h
+++ GITHUB.h
@@ -3,13 +3,20 @@ NEW REPOSITORY -> FOULDER OR PROJECT
 ls = dir  in windows
 .md // mark down
 //
+git --help // open help options
+
+git log // show all commits and mergs and all other actions
+git show //
 git config-- global user.name 'ANYNAMEINISNGLE COATS'
 git config --global user.email 'ANYEMAIL'
+
+git diff   // show diff between commit and un commit differece
+git diff branch_1 branch_2
 
 git --version // show version
 git  // show all commands
 git init // create an empty repository
-git clone http//
+git clone http  //    NICE TO WORK HERE
 git status
 git add file
 git add . 	// all every thing
@@ -32,6 +39,11 @@ git remote add origin https://github.com/201511608/Sample-app-for-tutorial.git /
 git push origin master
 git pull origin master
 
+git pull http   // After doing   git init
+git push http   // 
+git push --set-upstream http     master
+
+git clone https://github.com/201511608/Sample-app-for-tutorial.git  // create any file and do this and do programming
 
 esc  :wq   // full commit
 ctrl d // get out from cat
@@ -39,3 +51,9 @@ ctrl d // get out from cat
 vim  click i to get insert mode and then start inserting   esc and :wq
 
 touch .gitignore  // create and put any file name it will ignore that to add and commit  //file names :: log.txt  log.text  /dir2  *.txt
+
+
+
+// Notes
+// fetch will download any changes from the remote* branch, updating your repository data, but leaving your local* branch unchanged.
+// pull will perform a fetch and additionally merge the changes into your local branch.
