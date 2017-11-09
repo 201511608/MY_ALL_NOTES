730dfbf572032f6d908a5bae9d6d57dbd351ff8f
MAK_midas <muzafar.alikhan15@gmail.com>
2017-11-09 10:16:38 +0530

    updated

--- .Net.h
+++ .Net.h
@@ -1,6 +1,6 @@
 .NetFramework
 
- Net framework applications are multi-platform applications
+.Net framework applications are multi-platform applications
 Platform that helps to write the following applications 
 following languages: C#, C++, Visual Basic, Jscript,  and communicate with each other language
 Windows applications
