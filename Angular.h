6b90a55ca08e0aafda81e7c6e0d797e5ef2a9bf6
MAK <muzafar.alikhan15@gmail.com>
2018-10-01 18:36:25 +0530

    SOlIDSNAKE UPDATED: '

---
+++ Angular.h
@@ -0,0 +1,67 @@
+//ANGULAR
+
+// Content
+// 1 :: Basic
+// 2 :: Requirment
+// 3 :: Quick Start Angular
+// 4 :: Visual Studio Code 
+// 5 :: ng commands
+// 6 ::
+
+
+// 1
+// Basic
+	Angular is Frontend/Clint Framework.  // FrontEnd
+	Single Page Application (SPAs)
+	MangoDb  // Back end 
+
+	NodesJs
+	HTML
+	BootStrap
+
+	Injectable
+	USERS
+	
+	e2e // End to end testing
+		
+	//Files in Angular
+		.components.css
+		.components.html
+		.components.spec.ts // -Testing file
+		.components.ts  // Main
+		.module.ts
+		
+	//After any change Save it to update in WEB page
+// 2
+// Requirment
+	Nodejs  // https://nodejs.org/en/
+	npm (Node packet manager)  // https://www.npmjs.com/get-npm
+	Visual Studio code
+	Git bash
+
+// 3
+// Quick Start Angular
+	https://angular.io/guide/quickstart
+	https://youtu.be/KhzGSHNhnbI
+
+	npm install -g @angular/cli  // Set up the Development Environment
+	ng new my-app  		// Create a new project  // my-app or any name
+	cd my-app  			// Serve the application 
+	ng serve --open     // Serve the application 
+    
+	http://localhost:4200/  //In Web Browser
+	file->src->app->app.module.ts
+
+// 4
+// Visual Studio Code 
+	Ctrl + ` // Opens Command shell
+	File->Preferance->Setting // Change Default settings
+
+// 5
+// ng commands
+// Chech this is file /Pakage.json
+	"start": "ng serve",
+	"build": "ng build",
+	"test": "ng test",
+	"lint": "ng lint",
+	"e2e": "ng e2e"
